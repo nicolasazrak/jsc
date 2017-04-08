f5fa0924fafede1c969fddebfd2d0fbfa3902f3a
Nicolas Azrak <azrak.nicolas@gmail.com>
2017-04-08 19:02:32 -0300

    Rewrite as class

--- index.js
+++ index.js
@@ -6,103 +6,111 @@ const traverse    = require("babel-traverse").default;
 const wrapperCode = fs.readFileSync("runtime/wrapper.js").toString();
 
 
-function ensureDirectoryExistence(filePath) {
-  var dirname = path.dirname(filePath);
-  if (fs.existsSync(dirname)) {
-    return true;
+function processJS() {
+  
+}
+
+
+
+class JSC {
+
+  ensureDirectoryExistence(filePath) {
+    var dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+      return true;
+    }
+    this.ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
   }
-  ensureDirectoryExistence(dirname);
-  fs.mkdirSync(dirname);
-}
 
+  addToBundle(content) {
+    fs.appendFileSync(path.join(".jsc", "bundle.js"), content);
+  }
 
-function addToBundle(content) {
-  fs.appendFileSync(path.join(".jsc", "bundle.js"), content);
-}
+  processFile(filePath) {
+    const outPath = path.join(".jsc", filePath);
+    const metadataPath = path.join(".jsc", filePath + ".metadata.json");
 
+    try {
+      const cacheMTime = fs.statSync(outPath).mtime;
+      const currentMTime = fs.statSync(filePath).mtime;
 
-function processFile(filePath) {
-  const outPath = path.join(".jsc", filePath);
-  const metadataPath = path.join(".jsc", filePath + ".metadata.json");
-
-  try {
-    const cacheMTime = fs.statSync(outPath).mtime;
-    const currentMTime = fs.statSync(filePath).mtime;
-
-    if (cacheMTime > currentMTime) {
-      try {
-        addToBundle(fs.readFileSync(outPath));
-        return JSON.parse(fs.readFileSync(metadataPath));
-      } catch(e) {
-        if (e.code !== 'ENOENT') {
-          throw e;
+      if (cacheMTime > currentMTime) {
+        try {
+          this.addToBundle(fs.readFileSync(outPath));
+          return JSON.parse(fs.readFileSync(metadataPath));
+        } catch(e) {
+          if (e.code !== 'ENOENT') {
+            throw e;
+          }
         }
       }
-    }
-  } catch (e) {
-    if (e.code !== 'ENOENT') {
-      throw e;
-    }
-
-  }
-
-  var { ast, code } = babel.transformFileSync(filePath, {
-    plugins: [
-      "transform-es2015-arrow-functions",
-      "transform-es2015-destructuring",
-      "transform-es2015-modules-commonjs"
-    ]
-  });
-
-  const basePath = path.dirname(filePath);
-  const dependencies = [];
-  traverse(ast, {
-    CallExpression(nodePath) {
-      if (nodePath.node.callee.name === "require") {
-        const nextDependency = path.join(basePath, nodePath.node.arguments[0].value);
-        dependencies.push(nextDependency);
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        throw e;
       }
+
     }
-  });
 
-  const metadata = {
-    dependencies
-  };
+    var { ast, code } = babel.transformFileSync(filePath, {
+      plugins: [
+        "transform-es2015-arrow-functions",
+        "transform-es2015-destructuring",
+        "transform-es2015-modules-commonjs"
+      ]
+    });
 
-  code = wrapperCode.replace("__MODULE__CODE__", code);
-  code = code.replace("__MODULE_NAME__", filePath);
+    const basePath = path.dirname(filePath);
+    const dependencies = [];
+    traverse(ast, {
+      CallExpression(nodePath) {
+        if (nodePath.node.callee.name === "require") {
+          // TODO check value is a string
+          const nextDependency = path.join(basePath, nodePath.node.arguments[0].value);
+          dependencies.push(nextDependency);
+        }
+      }
+    });
 
-  ensureDirectoryExistence(outPath);
-  addToBundle(code);
-  fs.writeFileSync(outPath, code);
-  fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
+    const metadata = {
+      dependencies
+    };
 
-  return metadata;
-}
+    code = wrapperCode.replace("__MODULE__CODE__", code);
+    code = code.replace("__MODULE_NAME__", filePath);
 
+    this.ensureDirectoryExistence(outPath);
+    this.addToBundle(code);
+    fs.writeFileSync(outPath, code);
+    fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
 
-function parseTree(initialPath, callStack=[]) {
-  if (callStack.indexOf(initialPath) !== -1) {
-    throw new Error("Error requiring '" + initialPath + "'. It was already required by another module. It has a cyclic dependency");
+    return metadata;
   }
 
-  const { dependencies } = processFile(initialPath);
-  dependencies.forEach(d => parseTree(d, callStack.concat([initialPath])));
-}
+  parseTree(initialPath, callStack=[]) {
+    if (callStack.indexOf(initialPath) !== -1) {
+      throw new Error("Error requiring '" + initialPath + "'. It was already required by another module. It has a cyclic dependency");
+    }
 
-
-function createBundleFrom(initialPath) {
-  try {
-    fs.unlinkSync(path.join(".jsc", "bundle.js"));
-
-  } catch (e) {
-
+    const { dependencies } = this.processFile(initialPath);
+    dependencies.forEach(d => this.parseTree(d, callStack.concat([initialPath])));
   }
-  ensureDirectoryExistence(".jsc/bundle.js");
-  addToBundle(fs.readFileSync("runtime/runtime.js").toString());  
-  parseTree(initialPath);
-  addToBundle('require("'+ path.dirname(initialPath) +'")("' + initialPath +'")');
+
+  createBundleFrom(initialPath) {
+    try {
+      fs.unlinkSync(path.join(".jsc", "bundle.js"));
+
+    } catch (e) {
+
+    }
+    this.ensureDirectoryExistence(".jsc/bundle.js");
+    this.addToBundle(fs.readFileSync("runtime/runtime.js").toString());  
+    this.parseTree(initialPath);
+    this.addToBundle('require("'+ path.dirname(initialPath) +'")("' + initialPath +'")');
+  }
+
 }
 
 
-createBundleFrom("samples/a.js");
+const runner = new JSC();
+runner.createBundleFrom("samples/a.js");
